0730d9e76882f43cccbdc2e596dd79660316fbd8
Jolin Zhang <zhangru0026@gmail.com>
2016-05-12 15:24:54 -0500

    create the second activity and realize button connection

--- app/src/main/java/com/example/jonelezhang/note/write.java
+++ app/src/main/java/com/example/jonelezhang/note/write.java
@@ -1,38 +1,22 @@
 package com.example.jonelezhang.note;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-    public ImageButton write;
+public class write extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        write = (ImageButton) findViewById(R.id.write);
-        write.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,write.class));
-            }
-        });
-
-
-
-
+        setContentView(R.layout.activity_write);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_write, menu);
         return true;
     }
 
